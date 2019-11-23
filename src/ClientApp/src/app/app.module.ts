da8a085df3f722472b51e98f854d514872a65fa3
lehieu2017ct <lehieu2017ct@gmail.com>
2019-11-23 12:50:49 +0700

    fix css file (traditional )

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -5,10 +5,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
@@ -20,12 +16,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    //NavMenuComponent,
-    //HomeComponent,
-    //CounterComponent,
-    //FetchDataComponent
      AppComponent,
+     //NavMenuComponent,
+     //HomeComponent,
+     //CounterComponent,
+     //FetchDataComponent
      AdminLayoutComponent,
      AuthLayoutComponent,
   ],
@@ -38,11 +33,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     RouterModule,
     ComponentsModule,
     AppRoutingModule,
-    //RouterModule.forRoot([
-    //  //{ path: '', component: HomeComponent, pathMatch: 'full' },
-    //  { path: 'counter', component: CounterComponent },
-    //  { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-    //])
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
