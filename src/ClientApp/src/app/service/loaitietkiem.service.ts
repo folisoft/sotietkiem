614a8ccaa0f6f18954146b7b1a42473025c7c4f9
congvo21290 <congvc90@gmail.com>
2019-11-26 02:51:27 +0700

    create

--- src/ClientApp/src/app/service/loaitietkiem.service.ts
+++ src/ClientApp/src/app/service/loaitietkiem.service.ts
@@ -12,6 +12,18 @@ export class LoaiTietKiemService {
     getAll(): Observable <LoaiTietKiemModel[]> {
         return this._savingBookApi.get(`api/loaitietkiem`);
     }
+
+    create(model: LoaiTietKiemModel): Observable<any> {
+        return this._savingBookApi.post(`api/loaitietkiem`, model);
+    }
+
+    update(model: LoaiTietKiemModel) {
+        return this._savingBookApi.put(`api/loaitietkiem`, model);
+    }
+
+    delete(id: number) {
+        return this._savingBookApi.post(`api/loaitietkiem/delete?id=${id}`, null);
+    }
 }
 
 export class LoaiTietKiemModel {
--- src/Controllers/LoaiTietKiemController.cs
+++ src/Controllers/LoaiTietKiemController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SoTietKiem.Data;
 using SoTietKiem.Models;
+using SoTietKiem.Models.Dto;
 
 namespace SoTietKiem.Controllers
 {
@@ -16,14 +17,75 @@ namespace SoTietKiem.Controllers
     public class LoaiTietKiemController : ControllerBase
     {
         [HttpGet("")]
-        public IEnumerable<LoaiTietKiem> GetAll()
+        public async Task<IActionResult> GetAll()
         {
             var optionsBuilder = new DbContextOptions<DatabaseContext>();
             using (var db = new DatabaseContext(optionsBuilder))
             {
                 var loaiTietKiem = db.LoaiTietKiem.ToList();
 
-                return loaiTietKiem;
+                return Ok(loaiTietKiem);
+            }
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> Create(LoaiTietKiemDto model)
+        {
+            var optionsBuilder = new DbContextOptions<DatabaseContext>();
+            using (var db = new DatabaseContext(optionsBuilder))
+            {
+                var result = false;
+                var loaiTietKiemMoi = new LoaiTietKiem()
+                {
+                    TenLoaiTietKiem = model.TenLoaiTietKiem,
+                    LaiSuat = model.LaiSuat,
+                    NgayHieuLuc = model.NgayHieuLuc
+                };
+                db.LoaiTietKiem.Add(loaiTietKiemMoi);
+                result = db.SaveChanges() > 0;
+
+
+                return this.Ok(result);
+            }
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> Edit(LoaiTietKiemDto model)
+        {
+            var optionsBuilder = new DbContextOptions<DatabaseContext>();
+            using (var db = new DatabaseContext(optionsBuilder))
+            {
+                var result = false;
+                var loaiTietKiem = db.LoaiTietKiem.FirstOrDefault(ltk => ltk.Id == model.Id);
+                if (loaiTietKiem != null)
+                {
+                    loaiTietKiem.TenLoaiTietKiem = model.TenLoaiTietKiem;
+                    loaiTietKiem.LaiSuat = model.LaiSuat;
+                    loaiTietKiem.NgayHieuLuc = model.NgayHieuLuc;
+
+                    result = db.SaveChanges() > 0;
+                }
+
+                return this.Ok(result);
+            }
+        }
+
+        [HttpPost("delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var optionsBuilder = new DbContextOptions<DatabaseContext>();
+            using (var db = new DatabaseContext(optionsBuilder))
+            {
+                var result = false;
+                var loaiTietKiem = db.LoaiTietKiem.FirstOrDefault(ltk => ltk.Id == id);
+                if (loaiTietKiem != null)
+                {
+                    db.LoaiTietKiem.Remove(loaiTietKiem);
+                    result = db.SaveChanges() > 0;
+                }
+                
+
+                return this.Ok(result);
             }
         }
     }
